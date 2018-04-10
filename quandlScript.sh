24bc356fcbdbe3a9504551a31228748a7079df5f
Graham Lalor <grahamlalor@gmail.com>
2018-04-10 14:38:24 +0100

    Update quandlScript.sh

--- quandlScript.sh
+++ quandlScript.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 /usr/bin/anaconda/envs/py35/bin/pip install quandl
+/usr/bin/anaconda/envs/py35/bin/pip install azure-storage-blob
+/usr/bin/anaconda/envs/py35/bin/pip install azure-storage-file
+/usr/bin/anaconda/envs/py35/bin/pip install azure-storage-queue
